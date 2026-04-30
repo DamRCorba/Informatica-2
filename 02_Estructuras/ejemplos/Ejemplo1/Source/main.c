fe36ff69077c2fc78fb3bfbc18fc63dba9e3e119
Lucas <158571796+Pichi-Cell@users.noreply.github.com>
2026-04-29 21:06:55 -0400

    Agregado output a color en main.c

--- 02_Estructuras/ejemplos/Ejemplo1/Source/main.c
+++ 02_Estructuras/ejemplos/Ejemplo1/Source/main.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
-#include "../Headers/Mytypes.h"
+#include "./headers.h"
 
 /*Carga la informacion de color en una matriz de estructura RGB*/
 int main (){
 /*Declaracion de Variables*/
   unsigned int i,j;
   dotRGB_t image[320][240];
-  unsigned char randomr=0, randomg=10, randomb=20;
 
   /*Carga de datos en la Matriz*/
   for(i=0; i < sizeof(image[0])/3; i++){
     for (j=0; j< sizeof(image)/sizeof(image[0]);j++){
-        image[j][i].R = randomr++; // informacion de Rojo
-        image[j][i].G = randomg++; // informacion de Verde
-        image[j][i].B = randomb++; // informacion de Azul
+        /* rand() genera un numero aleatorio entre 0 y 32767, se usa el operador modulo para limitar su valor maximo a 255 */
+        image[j][i].R = rand() % 256; // informacion de Rojo
+        image[j][i].G = rand() % 256; // informacion de Verde
+        image[j][i].B = rand() % 256; // informacion de Azul
     }
   }
 /* Lectura de datos de la matriz*/
   for(i=0; i < sizeof(image[0])/3; i++){
     for (j=0; j< sizeof(image)/sizeof(image[0]); j++){
-      printf("R: %d,G: %d, B:%d\n\r", image[j][i].R,image[j][i].G,image[j][i].B );
+      /* Caracter de escape SGR \033[48(Seteo de background);2(Estilo de texto, no relevante);<R>;<G>;<B>m 
+      * luego se repite el caracter pero con argumento 0 para limpiar el setting de color antes de la proxima insercion */
+      printf("\033[48;2;%d;%d;%dm  \033[0m", image[j][i].R,image[j][i].G,image[j][i].B  );
     }
+    printf("\n");
   }
 
   return 0;
